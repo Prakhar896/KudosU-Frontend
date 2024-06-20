f5801101cafdf7bc5bd572f1e9d17671f4ae1d9e
KohKeira <kohkeira996@gmail.com>
2024-06-20 18:48:33 +0800

    Change button to menu burger

--- app/page.js
+++ app/page.js
@@ -8,34 +8,31 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "@/components/ui/drawer"
-import { Button } from "@/components/ui/button"
-
+} from "@/components/ui/drawer";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<Drawer direction = 'left'>
-  <DrawerTrigger >Open</DrawerTrigger>
-  <DrawerContent className='h-screen absolute left-0 mt-0 w-64 rounded-none'>
-
-    <DrawerHeader>
-
-      <DrawerDescription>choose action:</DrawerDescription>
-      <Button>Inbox</Button>
-      <Button>Send A Kudos!</Button>
-
-    
-    </DrawerHeader>
-    <DrawerFooter>
-      <Button>Log in</Button>
-      <DrawerClose>
-        <Button variant="outline">Cancel</Button>
-      </DrawerClose>
-    </DrawerFooter>
-  </DrawerContent>
-</Drawer>
-
+      <Drawer direction="left">
+        <DrawerTrigger className="absolute  top-0 left-0">
+          <Menu className="w-12 h-12" />
+        </DrawerTrigger>
+        <DrawerContent className="h-screen absolute left-0 mt-0 w-64 rounded-none">
+          <DrawerHeader>
+            <DrawerDescription>choose action:</DrawerDescription>
+            <Button>Affirmation Wrap</Button>
+            <Button>Check In</Button>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Profile</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </main>
   );
 }
