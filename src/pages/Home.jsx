46697bb8c5f612d8d64dd666846eced743366e3e
KohKeira <kohkeira996@gmail.com>
2024-06-21 00:31:12 +0800

    Details pop up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import SampleComponent from "../components/SampleComponent";
 import server from "../networking";
 import { Button } from "@/components/ui/button";
 import MenuDrawer from "@/components/menuDrawer";
+import DetailsPopUp from "@/components/detailsPopUp";
 function Home() {
   const [health, setHealth] = useState("Checking...");
 
@@ -20,6 +21,14 @@ function Home() {
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <MenuDrawer />
+        <DetailsPopUp
+          imgName={
+            "https://www.desicomments.com/wp-content/uploads/2017/02/Happy-Smiley-Image.jpg"
+          }
+          text={"Thank you so much for helping me to my work"}
+          isAnonymus={true}
+          from={"Keira"}
+        />
       </main>
     </>
   );
