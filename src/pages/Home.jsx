a55752c4934d56d60d9e756448493214fa10bcf0
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-06-21 09:05:19 +0800

    completed keira and prakhar merging

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import server from "../networking"
 import { Box, Center, Container, Flex, HStack, Heading, Image, List, ListItem, Spacer, Spinner, Text, VStack, Button } from '@chakra-ui/react'
 import { AddIcon } from '@chakra-ui/icons'
 import { Link } from 'react-router-dom'
+import MenuDrawer from '@/components/menuDrawer'
 
 function Home() {
     const [loading, setLoading] = useState("")
@@ -40,31 +41,34 @@ function Home() {
     }
 
     return (
-        <Box>
-            <HStack mb={"30px"}>
-                <Heading as={"h2"} textAlign={"left"}>Kudos!</Heading>
-                <Spacer />
-                <Link to={"/sendCompliment"}><Button><AddIcon color={"blue"} /></Button></Link>
-            </HStack>
-            <VStack spacing={"20px"}>
-                {
-                    Object.values(complimentsData).map((compliment, index) => {
-                        return (
-                            <Box key={index} bg={"gray.100"} p={"10px"} rounded={"10px"} alignItems={"center"} border={compliment["recipientAcknowledged"] ? "" : "3px solid teal"}>
-                                <HStack alignItems={"flex-start"} spacing={"15px"}>
-                                    <Image maxHeight={"100%"} maxWidth={"35%"} display={"block"} objectFit={"cover"} src={compliment["imgURL"]} rounded={"10px"} />
-                                    <VStack alignItems={"flex-start"} textAlign={"left"}>
-                                        <Text>{compliment["text"]}</Text>
-                                        <Text size={"xs"} color={"gray.500"}>From {compliment["from"]}</Text>
-                                    </VStack>
-                                </HStack>
-                            </Box>
-                        )
-                    })
-                }
-            </VStack>
-        </Box>
-     )
+        <>
+            <MenuDrawer />
+            <Box mt={"70px"}>
+                <HStack mb={"30px"}>
+                    <Heading as={"h2"} textAlign={"left"}>Kudos!</Heading>
+                    <Spacer />
+                    <Link to={"/sendCompliment"}><Button><AddIcon color={"blue"} /></Button></Link>
+                </HStack>
+                <VStack spacing={"20px"}>
+                    {
+                        Object.values(complimentsData).map((compliment, index) => {
+                            return (
+                                <Box key={index} bg={"gray.100"} p={"10px"} rounded={"10px"} alignItems={"center"} border={compliment["recipientAcknowledged"] ? "" : "3px solid teal"}>
+                                    <HStack alignItems={"flex-start"} spacing={"15px"}>
+                                        <Image maxHeight={"100%"} maxWidth={"35%"} display={"block"} objectFit={"cover"} src={compliment["imgURL"]} rounded={"10px"} />
+                                        <VStack alignItems={"flex-start"} textAlign={"left"}>
+                                            <Text>{compliment["text"]}</Text>
+                                            <Text size={"xs"} color={"gray.500"}>From {compliment["from"]}</Text>
+                                        </VStack>
+                                    </HStack>
+                                </Box>
+                            )
+                        })
+                    }
+                </VStack>
+            </Box>
+        </>
+    )
 }
 
 export default Home;
